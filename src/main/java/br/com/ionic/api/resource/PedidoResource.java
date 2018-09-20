21022b7d5100943b1b0c8233aeaf9478aec36843
Bruno Alves de Padua <solucaoelogica@gmail.com>
2018-09-20 16:08:50 -0300

    Inserindo pedido

--- src/main/java/br/com/ionic/api/resource/PedidoResource.java
+++ src/main/java/br/com/ionic/api/resource/PedidoResource.java
@@ -1,11 +1,18 @@
 package br.com.ionic.api.resource;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.ionic.api.domain.Pedido;
 import br.com.ionic.api.service.PedidoService;
@@ -21,5 +28,16 @@ public class PedidoResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Pedido obj = this.pedidoService.findPedido(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = pedidoService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/br/com/ionic/api/service/PedidoService.java
+++ src/main/java/br/com/ionic/api/service/PedidoService.java
@@ -1,11 +1,18 @@
 package br.com.ionic.api.service;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.ionic.api.domain.ItemPedido;
+import br.com.ionic.api.domain.PagamentoComBoleto;
 import br.com.ionic.api.domain.Pedido;
+import br.com.ionic.api.domain.enums.EstadoPagamento;
+import br.com.ionic.api.repository.ItemPedidoRepository;
+import br.com.ionic.api.repository.PagamentoRepository;
 import br.com.ionic.api.repository.PedidoRepository;
 import br.com.ionic.api.service.exception.ObjectNotFoundException;
 
@@ -14,9 +21,46 @@ public class PedidoService{
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido findPedido(Integer id){
 		Optional<Pedido> obj = this.pedidoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: " 
 				+ id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = pedidoRepository.save(obj);
+		
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		
+		return obj;
 	}
 }
