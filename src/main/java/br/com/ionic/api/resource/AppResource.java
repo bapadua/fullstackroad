51c6af8133eea15d42e6810ebe6c38b3ad136042
Bruno Alves de Padua <solucaoelogica@gmail.com>
2018-09-21 13:41:02 -0300

    Home adicionado fix

--- src/main/java/br/com/ionic/api/resource/AppResource.java
+++ src/main/java/br/com/ionic/api/resource/AppResource.java
@@ -1,5 +1,6 @@
 package br.com.ionic.api.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class AppResource {
 
+	@GetMapping
 	public String getHome() {
 		return "https://solucaologica.github.io/";
 	}
