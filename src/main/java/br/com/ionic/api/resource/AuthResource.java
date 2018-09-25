509a3457508099e53624db7927cd9f3b86bdbcd3
Bruno Alves de Padua <solucaoelogica@gmail.com>
2018-09-25 14:38:57 -0300

    Esqueci a senha

--- src/main/java/br/com/ionic/api/resource/AuthResource.java
+++ src/main/java/br/com/ionic/api/resource/AuthResource.java
@@ -1,15 +1,19 @@
 package br.com.ionic.api.resource;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ionic.api.domain.dto.EmailDTO;
 import br.com.ionic.api.security.JwtUtil;
 import br.com.ionic.api.security.UserSS;
+import br.com.ionic.api.service.AuthService;
 import br.com.ionic.api.service.UserService;
 
 @RestController
@@ -19,11 +23,20 @@ public class AuthResource {
 	@Autowired
 	private JwtUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@PostMapping("/refresh")
 	public ResponseEntity<Void> refreshToken(HttpServletResponse res){
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		res.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping("/forgot")
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO){
+		service.sendNewPassword(objDTO.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/ionic/api/service/EmailService.java
+++ src/main/java/br/com/ionic/api/service/EmailService.java
@@ -2,6 +2,7 @@ package br.com.ionic.api.service;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.ionic.api.domain.Cliente;
 import br.com.ionic.api.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage obj);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
