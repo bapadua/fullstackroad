0bf6a3694ac3fdd33605c727ab702bf88a27ef1b
Bruno Alves de Padua <solucaoelogica@gmail.com>
2018-09-25 11:14:02 -0300

    autenticação, token implementados

---
+++ src/main/java/br/com/ionic/api/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package br.com.ionic.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.ionic.api.domain.Cliente;
+import br.com.ionic.api.repository.ClienteRepository;
+import br.com.ionic.api.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+	
+}
